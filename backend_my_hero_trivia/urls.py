c8c9d9583cfee4ac2dce04f1601ef49b4c48db09
Chun Hin D. Tang <dtang955472@gmail.com>
2021-09-07 00:11:45 -0400

    urls setup

--- backend_my_hero_trivia/urls.py
+++ backend_my_hero_trivia/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from myapp.views import HighscoreView, OneHighscoreView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('highscore/', HighscoreView.as_view()),
+    path('highscore/<param>/', OneHighscoreView.as_view())
 ]
